f343de653985f78ed6246e425c1aa7387a4b1d9b
小李憨憨 <1308465141@qq.com>
2022-12-11 11:33:12 +0800

    [修改GC9A01驱动]

---
+++ disp_drivers/GC9A01.h
@@ -0,0 +1,25 @@
+#ifndef GC9A01_H
+#define GC9A01_H
+
+#ifdef SSUD_DISP_CONTROLLER_GC9A01
+
+#include <rtdevice.h>
+#include <rtthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GC9A01_INVERT_COLORS 1
+
+void ssud_GC9A01_init(void);
+void ssud_GC9A01_fill(rt_uint16_t x_start, rt_uint16_t y_start, rt_uint16_t x_end, rt_uint16_t y_end, void *pcolor);
+void ssud_GC9A01_set_orientation(rt_uint8_t orientation);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* SSUD_DISP_CONTROLLER_GC9A01 */
+
+#endif /*GC9A01_H*/
