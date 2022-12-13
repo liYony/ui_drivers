921f4ad39858532cde856c59bc279b87aa985fc5
liYony <941843540@qq.com>
2022-12-13 16:01:40 +0800

    [fix] 修改背光宏定义

--- driver_port/backlight_port.c
+++ driver_port/backlight_port.c
@@ -4,33 +4,32 @@
 
 struct rt_device_pwm *backlight_pwm;
 
+#ifdef SSUD_BACKLIGHT_SWITCH_PIN
+static void ssud_backlight_switch_init(void)
+{
+    rt_pin_mode(SSUD_BACKLIGHT_SWITCH_PIN, PIN_MODE_OUTPUT);
+}
+#endif /* SSUD_BACKLIGHT_SWITCH_PIN */
+
+#ifdef SSUD_BACKLIGHT_PWM_DEVNAME
+static void ssud_backlight_pwm_init(void)
+{
+    backlight_pwm = (struct rt_device_pwm *)rt_device_find(SSUD_BACKLIGHT_PWM_DEVNAME);
+}
+#endif /* SSUD_BACKLIGHT_PWM_DEVNAME */
+
 void ssud_backlight_init(void)
 {
 #if defined SSUD_BACKLIGHT_PWM
     ssud_backlight_pwm_init();
+    ssud_backlight_light_set(SSUD_BACKLIGHT_PWM_BRIGHTNESS);
 #elif defined SSUD_BACKLIGHT_SWITCH
     ssud_backlight_switch_init();
 #endif
-    ssud_backlight_light_set(SSUD_BACKLIGHT_PWM_BRIGHTNESS);
-}
-
-static void ssud_backlight_switch_init(void)
-{
-#ifdef SSUD_BACKLIGHT_SWITCH_PIN
-    rt_pin_mode(SSUD_BACKLIGHT_SWITCH_PIN, PIN_MODE_OUTPUT);
-#endif /* SSUD_BACKLIGHT_SWITCH_PIN */
-}
-
-static void ssud_backlight_pwm_init(void)
-{
-#ifdef SSUD_BACKLIGHT_PWM_DEVNAME
-    backlight_pwm = (struct rt_device_pwm *)rt_device_find(SSUD_BACKLIGHT_PWM_DEVNAME);
-#endif /* SSUD_BACKLIGHT_PWM_DEVNAME */
 }
 
 void ssud_backlight_light_set(rt_int8_t light)
 {
-    rt_uint32_t period = 1000000; /* 1ms */
     light = light >= 100 ? 100 : (light < 0 ? 0 : light);
 #ifdef SSUD_BACKLIGHT_ACTIVE_ISLOW
     light = 100 - light;
@@ -42,6 +41,7 @@ void ssud_backlight_light_set(rt_int8_t light)
     else
         rt_pin_write(SSUD_BACKLIGHT_SWITCH_PIN, PIN_LOW);
 #elif defined(SSUD_BACKLIGHT_PWM)
+    rt_uint32_t period = 1000000; /* 1ms */
     rt_pwm_set(backlight_pwm, SSUD_BACKLIGHT_PWM_CHANNEL, period, light * 10000);
     rt_pwm_enable(backlight_pwm, SSUD_BACKLIGHT_PWM_CHANNEL);
 #endif
--- driver_port/backlight_port.h
+++ driver_port/backlight_port.h
@@ -4,8 +4,6 @@
 #include <rtdevice.h>
 #include <rtthread.h>
 
-static void ssud_backlight_switch_init(void);
-static void ssud_backlight_pwm_init(void);
 void ssud_backlight_init(void);
 void ssud_backlight_light_set(rt_int8_t light);
 void ssud_backlight_bool_set(rt_uint8_t status);
